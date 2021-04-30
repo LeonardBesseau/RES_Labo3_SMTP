5d94c296e1cef118f8c2ca5d544078b414c40e14
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-04-30 15:27:43 +0200

    Correct and tested

--- src/main/java/ch/heigvd/res/mail/smtp/SmtpClient.java
+++ src/main/java/ch/heigvd/res/mail/smtp/SmtpClient.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.mail.smtp;
 import ch.heigvd.res.mail.model.mail.Message;
 
 import java.io.*;
+import java.net.ConnectException;
 import java.net.Socket;
 
 // Class representing the SMTP client that will send pranks to a SMTP server
@@ -13,6 +14,7 @@ public class SmtpClient {
 
     /**
      * Constructor of the class SmtpClient
+     *
      * @param smtpServerAddress address of SMTP server
      * @param serverPort        port of SMTP server
      */
@@ -23,67 +25,85 @@ public class SmtpClient {
 
     /**
      * Method for sending an email to the SMTP server
+     *
      * @param message message to send
      */
     public void sendMessage(Message message) throws IOException {
 
-        Socket socket = new Socket(smtpServerAddress, serverPort);
-        PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-        String line = reader.readLine();
+        try (Socket socket = new Socket(smtpServerAddress, serverPort);
+             PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"))) {
 
-        writer.printf("EHLO localhost\r\n");
-        line = reader.readLine();
-        if (!line.startsWith("250")) {
-            throw new IOException("SMTP error: " + line);
-        }
-
-        // Reading all the informations send by the server before writing something
-        while (line.startsWith("250-")) {
-            line = reader.readLine();
-        }
-
-        // Setting up sender
-        writer.write("MAIL FROM:" + message.getFrom() + "\r\n");
-        writer.flush();
-        line = reader.readLine();
-
-        // Setting up recipients
-        for (String to : message.getTo()) {
-            writer.write("RCPT TO:<" + to + ">\r\n");
+            String line = reader.readLine();
+            System.out.println("Connected to " + line);
+            writer.printf("EHLO localhost\r\n");
             writer.flush();
             line = reader.readLine();
-            // TODO check if RCPT is accepted
-        }
+            if (!line.startsWith("250")) {
+                throw new IOException("SMTP error: " + line);
+            }
 
-        // Setting up blind copies
-        for (String bcc : message.getBcc()) {
-            writer.write("RCPT TO:" + bcc + "\r\n");
+            // Reading all the informations send by the server before writing something
+            while (line.startsWith("250-")) {
+                line = reader.readLine();
+            }
+
+            // Setting up sender
+            writer.write("MAIL FROM:" + message.getFrom() + "\r\n");
             writer.flush();
             line = reader.readLine();
-            // TODO check if RCPT is accepted
+
+            // Setting up recipients
+            for (String to : message.getTo()) {
+                writer.write("RCPT TO:<" + to + ">\r\n");
+                writer.flush();
+                line = reader.readLine();
+                if (!line.startsWith("250")) {
+                    System.err.println("Could not send email to " + to + ". Reason " + line);
+                } else {
+                    System.out.println("Sending to " + to + ".");
+                }
+            }
+
+            // Setting up blind copies
+            for (String bcc : message.getBcc()) {
+                writer.write("RCPT TO:" + bcc + "\r\n");
+                writer.flush();
+                line = reader.readLine();
+                if (!line.startsWith("250")) {
+                    System.err.println("Could not send email to " + bcc + ". Reason " + line);
+                } else {
+                    System.out.println("Sending to (hidden) " + bcc + ".");
+                }
+            }
+
+            // Filling up body of email
+            writer.write("DATA\r\n");
+            writer.flush();
+            line = reader.readLine();
+            writer.write("Content-type: text/plain; charset=\"utf-8\"\r\n");
+            writer.write("From: " + message.getFrom() + "\r\n");
+
+            writer.write("To: " + message.getTo()[0] + " ");
+            for (int i = 1; i < message.getTo().length; i++) {
+                writer.write(", " + message.getTo()[i]);
+            }
+            writer.write("\r\n");
+
+            writer.flush();
+            writer.write(message.getContent());
+            writer.write("\r\n.\r\n"); // To indicate end of body
+            writer.flush();
+            line = reader.readLine();
+            writer.write("QUIT\r\n");
+            writer.flush();
+            System.out.println("DONE\n");
         }
-
-        // Filling up body of email
-        writer.write("DATA\r\n");
-        writer.flush();
-        line = reader.readLine();
-        writer.write("Content-type: text/plain; charset=\"utf-8\"\r\n");
-        writer.write("From: " + message.getFrom() + "\r\n");
-
-        writer.write("To: " + message.getTo()[0] + " ");
-        for (int i = 1; i < message.getTo().length; i++) {
-            writer.write(", " + message.getTo()[i]);
+        catch (ConnectException e) {
+            throw new RuntimeException("SMTP server is not available or reachable. " + e.getMessage());
         }
-        writer.write("\r\n");
-
-        writer.flush();
-        writer.write(message.getContent());
-        writer.write("\r\n.\r\n"); // To indicate end of body
-        writer.flush();
-        line = reader.readLine();
-        writer.write("QUIT\r\n");
-        writer.flush();
-        socket.close();
+        catch (IOException e) {
+            throw new RuntimeException("A problem occurred with IO. " + e.getMessage());
+        }
     }
 }
--- src/test/java/ch/heigvd/res/mail/smtp/SmtpClientTest.java
+++ src/test/java/ch/heigvd/res/mail/smtp/SmtpClientTest.java
@@ -1,4 +1,28 @@
 package ch.heigvd.res.mail.smtp;
 
+import ch.heigvd.res.mail.config.ConfigManager;
+import ch.heigvd.res.mail.model.prank.Prank;
+import ch.heigvd.res.mail.model.prank.PrankGenerator;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SmtpClientTest {
+    @Test
+    public void connectionFailureShouldThrow() throws IOException {
+        ConfigManager configManager = new ConfigManager("./test_files/smtp/serverNotResponding");
+        PrankGenerator prankGenerator = new PrankGenerator(configManager);
+        SmtpClient smtpClient = new SmtpClient(configManager.getSmtpServerAddress(), configManager.getServerPort());
+
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            for (Prank prank : prankGenerator.generatePranks()) {
+                smtpClient.sendMessage(prank.generateMessage());
+            }
+        });
+
+        assertEquals(exception.getMessage(), "SMTP server is not available or reachable. Connection refused (Connection refused)");
+    }
 }
